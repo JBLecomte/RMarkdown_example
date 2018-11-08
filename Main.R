90527ff68cbd9a2a9928f3bf5ee0204662a93410
JBLecomte <lecomte.jeanbaptiste@gmail.com>
2018-11-08 14:07:55 +0100

    rmd example

---
+++ Main.R
@@ -0,0 +1,15 @@
+################################################################################
+### Package to run rmd files
+################################################################################
+
+library('knitr')
+library('Rmarkdown')
+library('tidyverse')
+library('bookdown')
+library('ggplot2')
+library('kableExtra')
+
+# Going further
+library('devtools')
+# Install the latest development snapshot from GitHub
+devtools::install_github("crsh/papaja@devel")
